24dcc64f190a970a4461967d8d6850dd4dcc0caf
Lemmah <lemayiannakolah@gmail.com>
2017-12-18 01:07:23 +0300

    ch(jenkins): Correct minor spelling error

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -28,7 +28,7 @@ node {
     stage ("Install Application Dependencies") {
         sh '''
             . hc-venv/bin/activate
-            pip install -r healthchecks-clone/requirements.txt
+            pip install -r ~/healthchecks-clone/requirements.txt
             pip install mock
             deactivate
             '''

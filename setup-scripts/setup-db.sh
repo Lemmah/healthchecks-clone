b3717c4db5066614ed47027ccb985dc631c4489a
Lemmah <lemayiannakolah@gmail.com>
2017-12-18 01:51:15 +0300

    ch(jenkins): Alter the db cretion script

--- setup-scripts/setup-db.sh
+++ setup-scripts/setup-db.sh
@@ -4,13 +4,8 @@ set -x
 set -o errexit
 set -o pipefail
 
-# Ensure that we're on the app directory
-cd ~/healthchecksapp/healthchecks-clone
-
 # Use example local settings for db configs
 cp hc/local_settings.py.example hc/local_settings.py
-
-cd ~/healthchecksapp/healthchecks-clone
 
 # Run the migrate command
 ./manage.py makemigrations accounts admin api auth contenttypes payments sessions
